be7e8528033d6236120ea138062c671ad9ce9523
MateuszKwiat <mateusz.kwiat316@gmail.com>
2025-03-24 15:55:44 +0100

    fix for spacing between each column

--- src/ValuesVectorController.cpp
+++ src/ValuesVectorController.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <memory>
+#include <iostream>
 
 #include "ValuesVectorController.h"
 
@@ -14,13 +15,17 @@ ValuesVectorController::ValuesVectorController(const int amount, const sf::Vecto
 }
 
 void ValuesVectorController::initialize_vector(const int amount, const sf::Vector2u& window_size) {
-    float val_width = window_size.x / amount;
+    float val_width = (static_cast<float>(window_size.x) - (static_cast<float>(amount) - 1)) / static_cast<float>(amount);
     float temp_pos = 0.f;
     float temp_height = 10.f;
+    std::cout << "val width: " << val_width << std::endl;
+
     for (int i = 0; i < amount; i++) {
         float temp_value = value_distribution->operator()(*gen);
         this->push_back(std::make_unique<ValueSprite>(temp_value, sf::Vector2f(val_width, temp_value * static_cast<float>(window_size.y)),
                                                       sf::Vector2f(temp_pos, static_cast<float>(window_size.y))));
+
+        std::cout << "pos: " << temp_pos << std::endl;
         temp_pos += val_width + 1.f;
         temp_height += 10.f;
     }
